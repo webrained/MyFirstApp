9e11348ad7ff43630993676206af4d6b0b8baeb1
Arman Torosyan <webrained@armans-macbook-pro.local>
2018-09-29 12:42:51 +0400

    Initial Commit

---
+++ hustle-mode/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  hustle-mode
+//
+//  Created by Arman Torosyan on 9/29/18.
+//  Copyright © 2018 Arman Torosyan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
